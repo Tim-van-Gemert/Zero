997d2348ff301c509857a28975d18a15a16c39a0
Tim van Gemert <timvangemert@outlook.com>
2023-06-07 14:30:28 +0200

    changed layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,10 +26,11 @@ export default function Home() {
 
   return (
     <div className="flex flex-col  items-center h-full">
+      
       <div className="w-full flex flex-col">
         <div
           ref={titleRef}
-          className="w-full h-[300px] md:h-[350px] lg:h-[500px] flex justify-center relative items-center bg-top bg-[url('/truck.png')] bg-cover"
+          className="w-full h-[300px] md:h-[350px] lg:h-[500px] flex justify-center   relative items-center bg-top bg-[url('/truck.png')] bg-cover"
         >
           <motion.h1
             initial={{ opacity: 0, y: 20 }}
@@ -48,32 +49,34 @@ export default function Home() {
           </motion.h1>
           <div className="w-full h-full absolute z-10 flex justify-center items-center backdrop-brightness-[30%]"></div>
         </div>
-        <div className="w-full h-[50px] z-20 md:-mt-12 bg-[#E7F5F4]"></div>
+      <div className="w-full h-[50px] -mt-12  relative z-20  bg-[#E7F5F4]"></div>
+
       </div>
       <div className="w-full h-fit flex flex-col max-w-[1360px]">
         <motion.div
           ref={block1Ref}
           initial={{ opacity: 0, x: -20 }}
           animate={block1InView ? { opacity: 1, x: 0 } : {}}
-          className="my-24 w-full"
+          className="my-20 w-full"
         >
           <ImageBlock url={"/01.png"} imageLeft={true} />
-        </motion.div>
-        <motion.div
-          ref={block2Ref}
-          initial={{ opacity: 0, x: 20 }}
-          animate={block2InView ? { opacity: 1, x: 0 } : {}}
-          className="my-24"
-        >
-          <ImageBlock url={"/02.png"} imageLeft={false} />
         </motion.div>
         <motion.div 
         ref={block3Ref}
         initial={{ opacity: 0, y: 20 }}
         animate={block3InView ? { opacity: 1, y: 0 } : {}}
-        className="my-24">
+        className="my-20">
           <ImageSlider/>
         </motion.div>
+        <motion.div
+          ref={block2Ref}
+          initial={{ opacity: 0, x: 20 }}
+          animate={block2InView ? { opacity: 1, x: 0 } : {}}
+          className="my-20"
+        >
+          <ImageBlock url={"/02.png"} imageLeft={false} />
+        </motion.div>
+
       </div>
     </div>
   );
