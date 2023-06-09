d48b4b9991131d89e80d9da85308818359035c2b
Tim van Gemert <82870317+Tim-van-Gemert@users.noreply.github.com>
2023-06-09 20:42:52 +0200

    added analyticsa

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,13 @@
 import '@/styles/globals.css'
 import Header from '@/components/header'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
     <div className='flex flex-col overflow-x-hidden'>
       <Header />
       <Component {...pageProps} />
+      <Analytics />
     </div>
   )
 }
